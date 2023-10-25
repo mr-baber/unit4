49f605631fdd5baa21e5e66e7cfcbbbb0be903ac
Ryan Baber <rbaber@episcopalacademy.org>
2023-10-25 09:03:05 -0400

    deleted TextExercise/moved MathAlgs

--- TextExercise.java
+++
@@ -1,7 +0,0 @@
-public class TextExercise {
-    public static void main(String[] args) {
-        // add a print statement here and submit
-        System.out.println("Hello world!");
-        System.out.println("Hi there!");
-    }
-}
--- src/MathAlgorithms.java
+++ src/MathAlgorithms.java
@@ -1,3 +1,4 @@
+package src;
 public class MathAlgorithms{
     public static void main(String[] args) {
 
